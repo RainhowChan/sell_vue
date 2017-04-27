9342dd3bcfc20c4a9c4c07759dc4f015ccb66127
RainhowChan <rainhowchan@163.com>
2017-04-27 18:41:59 +0800

    商户评论部分完成

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -10,32 +10,121 @@
       <div class="content">
         <h1 class="title">{{food.name}}</h1>
         <div class="detail">
-          <span class="sell-count">月售{{food.sellCount}}份</span><span class="rating">好評率{{food.rating}}</span>
+          <span class="sell-count">月售{{food.sellCount}}份</span><span class="rating">好评率{{food.rating}}</span>
         </div>
         <div class="price">
-          <span class="now">&yen;{{food.price}}</span><span class="old"
-                                                            v-show="food.oldPrice">&yen;{{food.oldPrice}}</span>
+          <span class="now">&yen;{{food.price}}</span>
+          <span class="old" v-show="food.oldPrice">&yen;{{food.oldPrice}}</span>
+        </div>
+        <div class="cartcontrol-wrapper" v-show="food.count>0">
+          <v-cart-control :food="food"></v-cart-control>
+        </div>
+        <transition name="fade">
+          <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="putToCart">加入购物车</div>
+        </transition>
+      </div>
+      <v-split v-show="food.info"></v-split>
+      <div class="info" v-show="food.info">
+        <div class="title">商品介绍</div>
+        <div class="text">{{food.info}}</div>
+      </div>
+      <v-split v-show="food.ratings"></v-split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <v-rating-select :ratings="food.ratings" :desc="desc" :onlyContent="onlyContent" :selectType="selectType"
+                         @toSelectType="toSelectType" @toOnlyContent="toOnlyContent"></v-rating-select>
+        <div class="rating-wrapper">
+          <ul v-if="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img class="avatar" :src="rating.avatar" alt="" width="12" height="12">
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>
+                <span>{{rating.text}}</span>
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || food.ratings.length===0">
+            暂时没有评论哦！
+          </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script type='text/ecmascript-6'>
+  import Vue from 'vue';
   import BScroll from 'better-scroll';
+  import CartControl from '../cartcontrol/cartcontrol';
+  import Split from '../split/split';
+  import RatingSelect from '../ratingselect/ratingselect';
+  import moment from 'moment';
+  /* eslint-disable no-unused-vars */
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
+
   export default {
     props: {
       food: {
         type: Object
       }
     },
+    data() {
+      return {
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
+      };
+    },
     methods: {
       back() {
         this.$emit('back');
+      },
+      putToCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count = 1;
+        }
+      },
+      toSelectType(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      toOnlyContent(bool) {
+        this.onlyContent = bool;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      needShow(rateType, text) {
+        if (this.onlyContent && !text) { // 只显示有内容的评论开关打开  且没有文字内容
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return rateType === this.selectType;
+        }
       }
     },
     created() {
+      this.selectType = ALL;
+      this.onlyContent = false;
       this.$nextTick(() => {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.food, {
@@ -45,11 +134,23 @@
           this.scroll.refresh();
         }
       });
+    },
+    components: {
+      'v-cart-control': CartControl,
+      'v-split': Split,
+      'v-rating-select': RatingSelect
+    },
+    filters: {
+      formatDate(time) {
+        let day = moment(time);
+        return day.format('YYYY-M-D H:mm');
+      }
     }
   };
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
+  @import "../../common/style/mixin.styl";
   @import "./food.styl";
 </style>
 
--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,12 +1,144 @@
 <template>
-<div>I am ratings</div>
+  <div class="ratings" ref="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <v-star :size="36" :score="seller.serviceScore"></v-star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <v-star :size="36" :score="seller.foodScore"></v-star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <v-split></v-split>
+      <v-rating-select :ratings="ratings" :onlyContent="onlyContent" :selectType="selectType"
+                       @toSelectType="toSelectType" @toOnlyContent="toOnlyContent"></v-rating-select>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item" v-show="needShow(rating.rateType,rating.text)">
+            <div class="avatar">
+              <img :src="rating.avatar" alt="" width="24" height="24">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <v-star :size="24" :score="rating.score"></v-star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length>0">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommend" class="item">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type='text/ecmascript-6'>
-  export default {};
+  import Star from '../star/star';
+  import Split from '../split/split';
+  import BScroll from 'better-scroll';
+  import RatingSelect from '../ratingselect/ratingselect';
+  import moment from 'moment';
+  /* eslint-disable no-unused-vars */
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
+
+  const ERR_OK = 0;
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        ratings: [],
+        selectType: ALL,
+        onlyContent: true
+      };
+    },
+    created() {
+      this.$http.get('/api/ratings').then((res) => {
+        res = res.body;
+        if (res.errno === ERR_OK) {
+          this.ratings = res.data;
+          this.$nextTick(() => {
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$refs.ratings, {
+                click: true,
+                probeType: 3
+              });
+            } else {
+              this.scroll.refresh();
+            }
+          });
+        }
+      });
+    },
+    components: {
+      'v-star': Star,
+      'v-split': Split,
+      'v-rating-select': RatingSelect
+    },
+    methods: {
+      toSelectType(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      toOnlyContent(bool) {
+        this.onlyContent = bool;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      needShow(selectType, text) {
+        if (this.onlyContent && !text) { // 只显示有内容的评论开关打开  且没有文字内容
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return selectType === this.selectType;
+        }
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let day = moment(time);
+        return day.format('YYYY-M-D H:mm');
+      }
+    }
+  };
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
-
+  @import "../../common/style/mixin.styl";
+  @import "./ratings.styl";
 </style>
 
