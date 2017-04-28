97b605e8e7ddc01b777e494e39071c63e7b35935
RainhowChan <rainhowchan@163.com>
2017-04-28 14:57:54 +0800

    小球动画完成

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -18,8 +18,12 @@
       </div>
     </div>
     <div class="ball-container">
-      <div v-for="ball in balls" v-show="ball.show" class="ball">
-        <div class="inner inner-hook"></div>
+      <div v-for="ball in balls">
+        <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
+          <div class="ball" v-show="ball.show" >
+            <div class="inner inner-hook"></div>
+          </div>
+        </transition>
       </div>
     </div>
     <transition name="fold">
@@ -111,6 +115,42 @@
           return;
         }
         window.alert(`需要支付${this.totalPrice}元`);
+      },
+      beforeDrop(element) {
+        let count = this.balls.length;
+        while (count--) {
+          let ball = this.balls[count];
+          if (ball.show) {
+            let rect = ball.el.getBoundingClientRect();
+            let x = rect.left - 32;
+            let y = -(window.innerHeight - rect.top - 22);
+            element.style.display = '';
+            element.style.webkitTransform = `translate3d(0,${y}px,0)`;
+            element.style.transform = `translate3d(0,${y}px,0)`;
+            let inner = element.getElementsByClassName('inner-hook')[0];
+            inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+            inner.style.transform = `translate3d(${x}px,0,0)`;
+          }
+        }
+      },
+      dropping(element, done) {
+        /* eslint-disable no-unused-vars */
+        let rf = element.offsetHeight;// 触发浏览器重绘
+        this.$nextTick(() => {
+          element.style.webkitTransform = 'translate3d(0,0,0)';
+          element.style.transform = 'translate3d(0,0,0)';
+          let inner = element.getElementsByClassName('inner-hook')[0];
+          inner.style.webkitTransform = 'translate3d(0,0,0)';
+          inner.style.transform = 'translate3d(0,0,0)';
+          element.addEventListener('transitionend', done);
+        });
+      },
+      afterDrop(element) {
+        let ball = this.dropBalls.shift();
+        if (ball) {
+          ball.show = false;
+          element.style.display = 'none';
+        }
       }
     },
     computed: {
@@ -159,45 +199,6 @@
           });
         }
         return show;
-      }
-    },
-    transitions: {
-      drop: {
-        beforeEnter(element) {
-          let count = this.balls.length;
-          while (count--) {
-            let ball = this.balls[count];
-            if (ball.show) {
-              let rect = ball.el.getBoundingClientRect();
-              let x = rect.left - 32;
-              let y = -(window.innerHeight - rect.top - 22);
-              element.style.display = '';
-              element.style.webkitTransform = `translate3d(0,${y}px,0)`;
-              element.style.transform = `translate3d(0,${y}px,0)`;
-              let inner = element.getElementsByClassName('inner-hook')[0];
-              inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
-              inner.style.transform = `translate3d(${x}px,0,0)`;
-            }
-          }
-        },
-        enter(element) {
-          /* eslint-disable no-unused-vars */
-          let rf = element.offsetHeight;// 触发浏览器重绘
-          this.$nextTick(() => {
-            element.style.webkitTransform = 'translate3d(0,0,0)';
-            element.style.transform = 'translate3d(0,0,0)';
-            let inner = element.getElementsByClassName('inner-hook')[0];
-            inner.style.webkitTransform = 'translate3d(0,0,0)';
-            inner.style.transform = 'translate3d(0,0,0)';
-          });
-        },
-        afterEnter(element) {
-          let ball = this.dropBalls.shift();
-          if (ball) {
-            ball.show = false;
-            element.style.display = 'none';
-          }
-        }
       }
     },
     components: {
